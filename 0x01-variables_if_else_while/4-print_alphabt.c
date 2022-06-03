8c2d4d524fea6d3065f8d8a5cc1c472991f8e9ed
Paul <paulwem3000@gmail.com>
2022-06-03 10:50:09 -0700

    exemptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return - Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowcase = 'a';
+	while (lowcase <= 'z')
+	{
+		if ((lowcase != 'q') && (lowcase != 'e'))
+		{
+			putchar(lowcase);
+		}
+		lowcase++;
+	}
+	putchar('\n');
+	return (0);
+}
