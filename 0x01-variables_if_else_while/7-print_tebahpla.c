195fd80824c94b5d5d2eefd111be6624053d2e48
Paul <paulwem3000@gmail.com>
2022-06-03 11:30:39 -0700

    done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowverse = 'z';
+
+	while (lowverse >= a)
+	{
+		putchar(lowverse);
+		lowverse--;
+	}
+	putchar('\n');
+	return (0);
+}
