0402e5c9d62082a07cc4329d386ba0c67917241b
Paul <paulwem3000@gmail.com>
2022-06-07 13:54:23 -0700

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - A function that prints alphabet, in lower case followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char lowcase;
+
+	lowcase = 'a';
+
+	while (lowcase <= 'z')
+	{
+		_putchar('lowcase');
+		_putchar++;
+	}
+	_putchar('\n');
+}
