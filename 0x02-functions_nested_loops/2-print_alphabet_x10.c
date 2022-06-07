9badca6d66d8fdbb18531438cb0129f90c377ff2
Paul <paulwem3000@gmail.com>
2022-06-07 15:34:21 -0700

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,17 +8,20 @@
 
 void print_alphabet_x10(void)
 {
-	char low = 'a';
+	char low;
 
 	int i = 0;
 
-	while (i <= 10)
+	while (i < 10)
 	{
+	
+		low = 'a';
+
 		while (low <= 'z')
-		{
-			_putchar(low);
-			low++;
-		}
+			{
+				_putchar(low);
+				low++;
+			}
 		_putchar('\n');
 		i++;
 	}
