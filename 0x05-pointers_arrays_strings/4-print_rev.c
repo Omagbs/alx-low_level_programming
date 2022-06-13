2d6fe72f75a81f430674ed73a88a8d3e9724d645
Paul <paulwem3000@gmail.com>
2022-06-13 10:06:49 -0700

    done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-* print_rev - a function that prints a string, in reverse, followed by a new line.
-* @s - An input string
+* print_rev - a function that prints a string, in reverse,
+* followed by a new line.
+* @s: An input string
 * Return: Nothing.
 */
 
