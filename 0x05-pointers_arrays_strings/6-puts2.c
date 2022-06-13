93655456976df5a87d91d586887cfb00fa182f00
Paul <paulwem3000@gmail.com>
2022-06-13 11:07:22 -0700

    done

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
  * puts2 - a function that prints every other character of a string,
  * starting with the first character, followed by a new line.
  * @str: An input for char
@@ -9,12 +28,10 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
-	{
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
 		_putchar(str[i]);
-		i+=2;
-	}
+
 	_putchar('\n');
 }
