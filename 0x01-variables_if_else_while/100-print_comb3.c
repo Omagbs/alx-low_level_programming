ffb6755844bba98f3ca1a5d48d5aa56b4f11fad1
Paul <paulwem3000@gmail.com>
2022-06-13 12:28:57 -0700

    done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		for (int j = 0; j <= 9; j++)
+		{
+			if(j == i)
+			{
+				if ((j
+
+				{
+				putchar('i');
+				putchar('j');
+				putchar(',');
