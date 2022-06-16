a7b25473a716c350cbfe68598656294506b59bf9
Paul <paulwem3000@gmail.com>
2022-06-16 12:44:26 -0700

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize first letter of each word
+ * @s: string to manipulate
+ * Return: String
+ */
+
+char *cap_string(char *s)
+{
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 'a' + 'A';
+	i++;
+
+	while (s[i] != '\0')
+	{
+		if ((s[i] >= 'a' && s[i] <= 'z') && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n'))
+		{
+			s[i] = s[i] - 'a' + 'A';
+		}
+		i++;
+	}
+	return (s);
+}
