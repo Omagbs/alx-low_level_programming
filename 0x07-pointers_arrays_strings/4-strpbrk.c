246b7218e7e49aeb16afcdad3b18acf1afa72d6d
Paul <paulwem3000@gmail.com>
2022-06-20 09:45:32 -0700

    done

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#define NULL 0
+
+/**
+ * _strpbrk - return pointer to byte in s that matches a byte in accept
+ * @s: string to search
+ * @accept: target matches
+ * Return: pointer to index of string at first occurence
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				s = &s[i];
+				return (s);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
