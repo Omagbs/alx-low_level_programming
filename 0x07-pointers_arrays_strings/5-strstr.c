9fe50ad6d93eb3ec547d66caa08a6f892dbc511e
Paul <paulwem3000@gmail.com>
2022-06-21 08:46:04 -0700

    fixed betty documentation style

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,7 +2,7 @@
 #define NULL 0
 
 /**
- * strstr -  a function that locates a substring.
+ * _strstr - a function that locates a substring.
  * @haystack: an input string to search in
  * @needle: an input string to locate into string haystack
  * Return:  a pointer to the beginning of the located substring,
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i = 0, j, x;
 
-	if (needle[0] =='\0')
+	if (needle[0] == '\0')
 		return (haystack);
 
 	while (haystack[i] != '\0')
