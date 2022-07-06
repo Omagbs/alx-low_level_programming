ca0ef131fb3368f87847f5c36a2a71f2324e6f42
Paul <paulwem3000@gmail.com>
2022-07-06 09:12:07 -0700

    done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,10 +7,9 @@
  *  @f: function to execute
  *  Return: Nothing
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
-	return;
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
