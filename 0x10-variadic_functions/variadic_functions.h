a6091376cc1f69bac53ec222884fe784ffccca68
Paul <83482861+Omagbs@users.noreply.github.com>
2022-07-10 08:25:23 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,15 +6,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-/**
- * struct dt - struct
- * @letter: letter signifying data type
- * @func: Function ptr
- */
-typedef struct dt
-{
-	char letter;
-	void(*func)(ap);
-} datatype;
-
 #endif
